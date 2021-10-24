8e0a75270f02f31ef001c6702cd12dff866a102f
Wenz111 <50144145+Wenz111@users.noreply.github.com>
2021-10-24 16:25:43 +0800

    Update firebase database config

--- src/database/firebase/config.js
+++ src/database/firebase/config.js
@@ -1,9 +1,11 @@
 import { initializeApp } from "firebase/app";
+import { getDatabase, ref } from "firebase/database";
 
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
   projectId: process.env.FIREBASE_PROJECT_ID,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.FIREBASE_APP_ID,
@@ -11,3 +13,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Get a reference to the database service
+export const databaseReference = ref(getDatabase(app));
