20a554c4535e673833c39a1a2c3d1dd6695909dc
Wenz111 <azwenz11@gmail.com>
2021-10-27 11:50:29 +0800

    Add application guild commands for instant update for development purpose and comment out application global guild commands

--- src/slash-commands.js
+++ src/slash-commands.js
@@ -7,13 +7,29 @@ const commands = await GetWhitelistPikaBotCommands();
 
 const rest = new REST({ version: "9" }).setToken(process.env.AUTH_TOKEN);
 
+const guildIds = [
+  process.env.GUILD_ID_RSF_FLOBBIES,
+  process.env.GUILD_ID_KNOCK_KNOCK,
+];
+
 (async () => {
   try {
     console.log("Started refreshing application (/) commands.");
 
-    await rest.put(Routes.applicationCommands(process.env.CLIENT_ID), {
-      body: commands,
-    });
+    // Guild Commands - instant update for development purpose
+    for (const guildId of guildIds) {
+      await rest.put(
+        Routes.applicationGuildCommands(process.env.CLIENT_ID, guildId),
+        {
+          body: commands,
+        }
+      );
+    }
+
+    // Global Guild Commands - cached for one hour
+    // await rest.put(Routes.applicationCommands(process.env.CLIENT_ID), {
+    //   body: commands,
+    // });
 
     console.log("Successfully reloaded application (/) commands.");
   } catch (error) {
